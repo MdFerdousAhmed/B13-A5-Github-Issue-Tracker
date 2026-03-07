8c78add3e6cdedc74ae37f08867307bb0518d5fb
MD FERDOUS AHMED <148644652+MdFerdousAhmed@users.noreply.github.com>
2026-03-07 07:55:53 +0600

    add sign in button

---
+++ script/sign.js
@@ -0,0 +1,30 @@
+document.getElementById('sign-btn').addEventListener("click", function () {
+
+  // 1. get the mobile number
+  const usernameInput = document.getElementById('input-username');
+  const userName = usernameInput.value;
+  console.log(userName);
+  
+  // 2. get the pin
+  const inputPin = document.getElementById('input-pin')
+  const pin = inputPin.value;
+  console.log(pin);
+  
+  // 3. matched pin & mobile number
+  if(userName == "admin" && pin =="admin123"){
+
+    // 3-1. true : >> alert > homepage
+    alert("login Success")
+    // window.location.replace("/home.html");
+    window.location.assign("/home.html");
+  }
+
+  else{
+
+    // 3-1. false : >> alert > return
+    alert("login Failed")
+    return;
+  }
+
+
+})
